b83d2b954516c3a97d033a6dbad1df0d580b0092
Joran Kikke <joran.k@gmail.com>
2015-11-07 11:40:51 +1300

    manual ignore fonts for asset sync

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,13 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+AssetSync.config.ignored_files = [
+  'glyphicons-halflings-regulat.eot',
+  'glyphicons-halflings-regulat.svg',
+  'glyphicons-halflings-regulat.ttf',
+  'glyphicons-halflings-regulat.woff',
+  'glyphicons-halflings-regulat.woff2'
+]
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )

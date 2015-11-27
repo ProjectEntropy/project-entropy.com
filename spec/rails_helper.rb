23f501fc5c094653b14a277f9d0dfa54981eb009
Joran Kikke <joran.k@gmail.com>
2015-11-27 21:13:06 +1300

    vcr settings for travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,9 +28,17 @@ require 'vcr'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/vcr_cassettes"
-  config.hook_into :webmock # or :fakeweb
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/vcr_cassettes"
+  c.hook_into :webmock # or :fakeweb
+  c.configure_rspec_metadata!
+
+  c.default_cassette_options = {
+    :serialize_with             => :json,
+    :preserve_exact_body_bytes  => true,
+    :decode_compressed_response => true,
+    :record                     => ENV['TRAVIS'] ? :none : :once
+  }
 end
 
 

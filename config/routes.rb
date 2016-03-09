ff854cbbc2f81a2925a04734942b35769e57046a
Joran <joran.k@gmail.com>
2016-03-10 10:37:30 +1300

    changes to crowdfund

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "crowdfund", controller: "crowdfund", action: "index"
-  get 'crowdfund_data/:metric', to: 'crowdfund_data#show'
-  
   root to: 'visitors#index'  
 end

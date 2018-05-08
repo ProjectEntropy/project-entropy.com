a2ce6506099a118025535d75c97f19731f8afcd7
Joran <joran.k@gmail.com>
2018-05-08 14:36:29 +1200

    get rid of old fog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 end
